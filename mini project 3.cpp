eb580e66cf1f7f49efdf78cc4368a5dff9e23654
Iqra2464 <iqrat6434@gmailo.com>
2024-11-19 20:30:33 -0800

    first project

---
+++ mini project 3.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+#include <unistd.h>
+using namespace std;
+
+const int SCREEN_WIDTH = 10;
+const int FALL_SPEED = 500000;  
+const int NUM_OBJECTS = 5;
+
+struct object {
+    int x;
+    char symbol;
+    bool active;
+};
+
+void displayScreen(object objects[], int playerPos) {
+    system("clear");  
+    
+    for (int i = 0; i < SCREEN_WIDTH; i++) {
+        bool objecthere = false;
+        
+        for (int j = 0; j < NUM_OBJECTS; j++) {
+            if (objects[j].active && objects[j].x == i) {
+                cout << objects[j].symbol;
+                objecthere = true;
+                break;  
+            }
+        }
+
+        if (!objecthere) {
+            cout << ".";
+        }
+    }
+
+    cout << "\nPlayer position: " << playerPos << endl;
+}
+
+void initializeObjects(object objects[]) {
+
+    for (int i = 0; i < NUM_OBJECTS; i++) {
+        objects[i].x = rand() % SCREEN_WIDTH;
+        objects[i].symbol = 'A' + i;  
+        objects[i].active = true;
+    }
+}
+
+int main() {
+    srand(time(0));  
+
+    object objects[NUM_OBJECTS];
+    initializeObjects(objects);
+
+    int playerScore = 0;
+    int rounds = 5;
+
+    while (rounds > 0) {
+        displayScreen(objects, playerScore);
+        
+        
+        for (int i = 0; i < NUM_OBJECTS; i++) {
+            if (objects[i].active) {
+                char input;
+                cout << "Catch the object by typing its letter (" << objects[i].symbol << "): ";
+                cin >> input;
+                
+                
+                if (input == objects[i].symbol) {
+                    cout << "Caught!" << endl;
+                    objects[i].active = false;  
+                    playerScore++;
+                } else {
+                    cout << "Missed!" << endl;
+                }
+            }
+        }
+
+        rounds--;  
+        usleep(FALL_SPEED);  
+    }
+
+    
+    cout << "Game Over! Your score: " << playerScore << endl;
+    return 0;
+}
+
